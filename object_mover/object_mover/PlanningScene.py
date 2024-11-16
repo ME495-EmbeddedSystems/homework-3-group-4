4595c83ae3783eba64355376602a5a01d3a53da6
haodong <haodong0518@gmail.com>
2024-11-15 22:21:48 -0600

    detach needs updated info

--- object_mover/object_mover/PlanningScene.py
+++ object_mover/object_mover/PlanningScene.py
@@ -1,5 +1,5 @@
 import rclpy
-from moveit_msgs.srv import GetPlanningScene
+from moveit_msgs.srv import GetPlanningScene, ApplyPlanningScene
 from moveit_msgs.msg import PlanningScene as MoveitPlanningScene
 from moveit_msgs.msg import CollisionObject, AttachedCollisionObject
 from geometry_msgs.msg import Pose
@@ -13,10 +13,13 @@ class PlanningScene:
         self.get_scene = self.node.create_client(GetPlanningScene, '/get_planning_scene')
         while not self.get_scene.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('get_scene service not available, waiting again...')
+
+        self.apply_scene = self.node.create_client(ApplyPlanningScene, '/apply_planning_scene')
+        while not self.apply_scene.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('apply_scene service not available, waiting again...')
  
         self.objects = {}  
-        self.scene_publisher = self.node.create_publisher(MoveitPlanningScene, '/planning_scene', 10)
-
+        self.attach_objects = {}
     async def add_collision_objects(self, name, position, dimension):
         """
         add the collision object to the environment
@@ -44,7 +47,8 @@ class PlanningScene:
         collision_object.primitive_poses.append(box_pose)
         collision_object.operation = CollisionObject.ADD
         self.scene_response.scene.world.collision_objects.append(collision_object)
-        self.scene_publisher.publish(self.scene_response.scene)
+        response = await self.apply_scene.call_async(ApplyPlanningScene.Request(scene=self.scene_response.scene))
+        return response
 
     async def remove_box(self, name):
         """
@@ -55,13 +59,27 @@ class PlanningScene:
 
         """
         self.scene_response  = await self.get_scene.call_async(GetPlanningScene.Request())      
-        for object in self.scene_response.scene.world.collision_objects:
-            if object.id == name:
-                collision_object = object
+        try:
+            for object in self.scene_response.scene.world.collision_objects:
+                if object.id == name:
+                    collision_object = object
+            self.scene_response.scene.world.collision_objects.remove(collision_object)
 
-        self.scene_response.scene.world.collision_objects.remove(collision_object)
-        self.scene_publisher.publish(self.scene_response.scene)
+        except:
+            self.node.get_logger().info('------------------The object is not in the collision objects------------------')
 
+        try:
+            for object_attached in self.scene_response.scene.robot_state.attached_collision_objects:
+                if object_attached.object.id == name:
+                    object_remove = object_attached
+            self.scene_response.scene.robot_state.attached_collision_objects.remove(object_remove)
+
+        except:
+            self.node.get_logger().info('------------------The object is not in the attached collision objects------------------')
+
+        response = await self.apply_scene.call_async(ApplyPlanningScene.Request(scene=self.scene_response.scene))
+        return response
+    
     async def attach_object(self, linkname, name):
         """
         attach the box to the end-effector 
@@ -72,25 +90,36 @@ class PlanningScene:
         self.scene_response  = await self.get_scene.call_async(GetPlanningScene.Request())        
         if name in self.objects:
             collision_object = self.objects[name]
+        for object in self.scene_response.scene.world.collision_objects:
+            if object.id == name:
+                self.scene_response.scene.world.collision_objects.remove(object)
+                self.attach_objects[name] = object
+        del self.objects[name]
         attach_object = AttachedCollisionObject()
         attach_object.object = collision_object
         attach_object.link_name = linkname
         self.scene_response.scene.robot_state.attached_collision_objects.append(attach_object)
-        self.scene_publisher.publish(self.scene_response.scene)
-
+        response = await self.apply_scene.call_async(ApplyPlanningScene.Request(scene=self.scene_response.scene))
+        return response
+    
     async def detach_object(self, name):
         """
         detach the box from the robot and add reintroduce the object into the environment
         Args:
             the box name 
         """
-        self.scene_response  = await self.get_scene.call_async(GetPlanningScene.Request())        
-        for object in self.scene_response.scene.robot_state.attached_collision_objects:
-            if object.object.id == name:
-                detach_object = object
+        self.scene_response  = await self.get_scene.call_async(GetPlanningScene.Request())
+        try:        
+            for object in self.scene_response.scene.robot_state.attached_collision_objects:
+                if object.object.id == name:
+                    detach_object = object
+            self.scene_response.scene.world.collision_objects.append(detach_object.object)
+        except:
+            self.node.get_logger().info('-------------The object is not found in the attached objects------------------')
         self.scene_response.scene.robot_state.attached_collision_objects.remove(detach_object)
-        self.scene_publisher.publish(self.scene_response.scene)
-
+        response = await self.apply_scene.call_async(ApplyPlanningScene.Request(scene=self.scene_response.scene))
+        return response
+    
     async def clear_scene(self):
         """
         clear the scene
@@ -98,4 +127,6 @@ class PlanningScene:
         self.scene_response  = await self.get_scene.call_async(GetPlanningScene.Request())     
         self.scene_response.scene.world.collision_objects.clear()
         self.scene_response.scene.robot_state.attached_collision_objects.clear()
-        self.scene_publisher.publish(self.scene_response.scene)
+        response = await self.apply_scene.call_async(ApplyPlanningScene.Request(scene=self.scene_response.scene))
+        return response
+    
