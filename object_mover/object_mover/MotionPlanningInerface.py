35dc38694a35c633b51f45955669347b5054aa93
sairam-umakanth <sairamumakanth2028@u.northwestern.edu>
2024-11-18 21:44:38 -0600

    Fixed flake8 errors for RobotState, MPI and pick_node

--- object_mover/object_mover/MotionPlanningInerface.py
+++ object_mover/object_mover/MotionPlanningInerface.py
@@ -1,44 +1,60 @@
-import rclpy
-import rclpy.callback_groups
-from rclpy.node import Node
-from moveit_msgs.action import MoveGroup
+"""
+Motion planning interface for moveit API.
+
+This class incorporates MotionPlanner, RobotState and PlanningScene classes,
+offering an interface for a user to interact with the moveit API.
+"""
+
+from typing import Dict, List, Optional
+
 from geometry_msgs.msg import Pose
-from object_mover.RobotState import RobotState as CustomRobotState
+
+from moveit_msgs.msg import MotionPlanRequest
+
 from object_mover.MotionPlanner import MotionPlanner
 from object_mover.PlanningScene import PlanningScene
-from moveit_msgs.msg import RobotState as MoveitRobotState
-from moveit_msgs.msg import MotionPlanRequest
-from typing import Optional, List, Dict
+from object_mover.RobotState import RobotState as CustomRobotState
+
+from rclpy.node import Node
 
 
 class MotionPlanningInterface:
-    '''
-    A class that consolidates MotionPlanner, RobotState and PlanningScene classes in one class
-    that is able to be utilized by any user node for any planning and execution task.
-    '''
+    """
+    Main class interacting with moveit API.
+
+    A class that consolidates MotionPlanner, RobotState and PlanningScene
+    classes in one class that is able to be utilized by any user node
+    for any planning and execution task.
+    """
+
     def __init__(self, node: Node):
-        '''
-        Initializes MotionPlanningInterface with an instance of the ROS node passed to it as
-        an argumet
-        '''
+        """
+        Create objects of other classes.
+
+        Initializes MotionPlanningInterface with an instance of the ROS node
+        passed to it as an argumet
+        """
         self.node = node
-        self.planning_scene = PlanningScene(node = self.node)
-        self.robot_state = CustomRobotState(node = self.node)
-        self.motion_planner = MotionPlanner(node = self.node, robot_state = self.robot_state, planning_scene= self.planning_scene)
+        self.planning_scene = PlanningScene(node=self.node)
+        self.robot_state = CustomRobotState(node=self.node)
+        self.motion_planner = MotionPlanner(
+            node=self.node,
+            robot_state=self.robot_state,
+            planning_scene=self.planning_scene)
         self.path = MotionPlanRequest()
-        
+
     async def plan_path(
         self,
-        start_joints: Optional[List[float]] = None, 
-        goal_joints: Optional [Dict[str, float]] = None,
-        start_pose: Optional[Pose] = None, 
+        start_joints: Optional[List[float]] = None,
+        goal_joints: Optional[Dict[str, float]] = None,
+        start_pose: Optional[Pose] = None,
         goal_pose: Optional[Pose] = None,
         waypoints: List[Pose] = None,
         named_configuration: str = None,
-        ):
-        '''
-        Function to plans path according to the inputs passed, uses MotionPlanner functions
-        to plan path.
+            ):
+        """
+        Plan path using MotionPlanner class.
+
         :param start_joints: starting joint configuration for path planning
         :type start_joints: list[float]
         :param goal_joints: goal joint configuration for path planning
@@ -49,32 +65,33 @@ class MotionPlanningInterface:
         :type goal_pose: geometry_msgs.msg.Pose
         :param waypoints: list of waypoints for cartesian path planning
         :type waypoints: list[geometry_msgs.msg.Pose]
-        :param named_configuration: named configuration input passed for path planning
+        :param named_configuration: named configuration inputfor path planning
         :type named_configuration: string
         :returns: planned path
         :rtype: moveit_msgs.msg.MotionPlanRequest
-        '''
+        """
         if goal_joints is not None:
             self.path = await self.motion_planner.plan_joint_path(goal_joints)
         elif goal_pose is not None:
             self.path = await self.motion_planner.plan_pose_to_pose(
                 start_pose,
-                goal_pose, execute = True
+                goal_pose, execute=True
             )
         elif waypoints is not None:
-            self.path = await self.motion_planner.plan_cartesian_path(waypoints)
+            self.path = await self.motion_planner.plan_cartesian_path(
+                waypoints)
 
         return self.path
 
     async def exec_path(self, path: MotionPlanRequest = None):
-        '''
-        Executes calculated path 
+        """
+        Execute calculated path.
 
         :param path: path planned for execution
         :type path: moveit_msgs.msg.MotionPlanRequest
         :returns: result of execution
         :rtype: bool
-        '''
+        """
         if path is None:
             # Error message
             result = 0
--- object_mover/object_mover/pick_node.py
+++ object_mover/object_mover/pick_node.py
@@ -1,85 +1,102 @@
-'''
-The pick node is a node that accomplishes the pick and place task of an object by the robotic arm. It directly
-interacts with MotionPlanningInterface to access the functions of the objects of MotionPlanner, RobotState and 
-PlanningScene incorporated within the MotionPlanningInterface class. 
-'''
+"""
+Node that conducts pick and drop task.
 
-import time
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Empty
-from object_mover.MotionPlanningInerface import MotionPlanningInterface
-from object_mover_interfaces.srv import PickPose
-from geometry_msgs.msg import Pose
-from enum import Enum, auto
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
-from rclpy.action import ActionServer
-from moveit_msgs.action import MoveGroup
+The pick node is a node that accomplishes the pick and place task of an object
+by the robotic arm. It directly interacts with MotionPlanningInterface to
+access the functions of the objects of MotionPlanner, RobotState and
+PlanningScene incorporated within the MotionPlanningInterface class.
+"""
+
 import copy
+from enum import Enum, auto
+
+from geometry_msgs.msg import Pose
+
+from moveit_msgs.action import MoveGroup
+
+from object_mover.MotionPlanningInerface import MotionPlanningInterface
+
+from object_mover_interfaces.srv import PickPose
+
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.node import Node
+
 
 class State(Enum):
-    '''
-    Current state of the system (might not be needed)
-    '''
+    """Current state of the system (might not be needed)."""
+
     MOVING = auto()
     STATIONARY = auto()
 
 
 class PickNode(Node):
-    '''
-    Node that interacts with the moveit API to conduct pick and place task with robot arm
-    '''
+    """
+    Node that conducts pick and drop task.
+
+    Node that interacts with the moveit API to conduct pick and place task
+    with robot arm.
+    """
+
     def __init__(self):
+        """Define parameters."""
         super().__init__('pick_node')
         self._cbgroup = MutuallyExclusiveCallbackGroup()
-        self.serv = self.create_service(PickPose, 'pick', self.pick_callback, callback_group=MutuallyExclusiveCallbackGroup())
+        self.serv = self.create_service(
+            PickPose,
+            'pick',
+            self.pick_callback,
+            callback_group=MutuallyExclusiveCallbackGroup())
         self._server = ActionServer(self,
                                     MoveGroup,
                                     '/viz/move_action',
                                     self.move_action_callback,
-                                    callback_group = self._cbgroup)
+                                    callback_group=self._cbgroup)
         self.state = State.STATIONARY
         self.mpi = MotionPlanningInterface(self)
 
-    # Maybe need to create custom service type for pick service as we want to be able to load multiple things to it
     async def pick_callback(self, request: PickPose, response):
-        '''
-        Callback function when pick service is triggered
+        """
+        Execute pick taskflow.
 
         :param request: service request object
         :type request: object_mover_interfaces.srv.PickPose
         :returns: response to service call
         :rtype: bool
-        '''
-        # For now I am assuming we are locating pose of object as the input to pick service 
+        """
         box_pose = Pose()
         box_pose.position.x = 0.59
         box_pose.position.y = 0.13
         box_pose.position.z = 0.1378
-        await self.mpi.planning_scene.add_collision_objects('box', box_pose, [0.05, 0.05, 0.05])
+        await self.mpi.planning_scene.add_collision_objects(
+            'box',
+            box_pose,
+            [0.05, 0.05, 0.05])
 
         wall_pose = Pose()
         wall_pose.position.x = 0.5
         wall_pose.position.y = 0.3
         wall_pose.position.z = 0.0
 
-        await self.mpi.planning_scene.add_collision_objects('wall', wall_pose, [0.05, 0.05, 1.0])
+        await self.mpi.planning_scene.add_collision_objects(
+            'wall',
+            wall_pose,
+            [0.05, 0.05, 1.0])
         object_pose = Pose()
         object_pose = request.pick_point
         pose1 = copy.deepcopy(object_pose)
 
         pose1.position.z = object_pose.position.z + 0.4
-        plan = await self.mpi.plan_path(goal_pose = pose1) 
-        # await self.mpi.exec_path(path = plan)
+        await self.mpi.plan_path(goal_pose=pose1)
 
         await self.mpi.motion_planner.toggle_gripper('open')
 
         pose2 = object_pose
-        plan = await self.mpi.plan_path(goal_pose = pose2)
-        # await self.mpi.exec_path(path = plan)
+        await self.mpi.plan_path(goal_pose=pose2)
 
-        await self.mpi.motion_planner.toggle_gripper('close')
         # Closing grippers
+        await self.mpi.motion_planner.toggle_gripper('close')
 
         # Attaching box to arm in scene
         await self.mpi.planning_scene.attach_object('box')
@@ -87,39 +104,42 @@ class PickNode(Node):
         # Lifts object slighty off table
         pose3 = object_pose
         pose3.position.z = object_pose.position.z + 0.3
-        plan = await self.mpi.plan_path(goal_pose = pose3)
-        # await self.mpi.exec_path(path = plan)
+        await self.mpi.plan_path(goal_pose=pose3)
+
         # Move arm to other side of obstacle
         pose4 = object_pose
         pose4.position.y = object_pose.position.y + 0.3
-        plan = await self.mpi.plan_path(goal_pose= pose4)
-        #await self.mpi.exec_path(path = plan)
-        await self.mpi.motion_planner.toggle_gripper('open')
+        await self.mpi.plan_path(goal_pose=pose4)
+
         # Drop object
+        await self.mpi.motion_planner.toggle_gripper('open')
         return response
 
     async def move_action_callback(self, goal_handle):
-        self.get_logger().info("Move Action Called. Goal Is")
-        # self.get_logger().info(f"{goal_handle.request}") # Do more to format this output nicelynicely
-        self.get_logger().info(f"--- End ofrequest dump.----\n")
-        self.get_logger().info("Forwarding the action to the move_group action server")
-       
-        self.get_logger().info("Awaiting the result")
-        self.get_logger().info("Interception successful, returning the result")
+        """
+        Move action callback.
+
+        :param goal_handle: goal handle of move action
+        :type goal_handle:
+        :rtype: bool
+        """
+        self.get_logger().info('Move Action Called. Goal Is')
+        self.get_logger().info('--- End ofrequest dump.----\n')
+        self.get_logger().info(
+            'Forwarding the action to the move_group action server')
+
+        self.get_logger().info('Awaiting the result')
+        self.get_logger().info('Interception successful, returning the result')
         return
 
 
 def pick_entry(args=None):
-    '''main function'''
+    """Execute main function."""
     rclpy.init(args=args)
     node = PickNode()
     rclpy.spin(node)
-    rclpy.shutdown()  
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     pick_entry()
-
-
-
-
-
