10f5d016ce36c38772c70c9a8eb38669953cf080
haodong <haodong0518@gmail.com>
2024-11-21 15:13:28 -0600

    Fix more flake 8

--- object_mover/object_mover/RobotState.py
+++ object_mover/object_mover/RobotState.py
@@ -1,15 +1,17 @@
-from moveit_msgs.srv import GetPositionFK, GetPositionIK
-from sensor_msgs.msg import JointState
-from rclpy.node import Node
-from moveit_msgs.msg import RobotState as MoveitRobotState
 from geometry_msgs.msg import PoseStamped
+
+from moveit_msgs.msg import RobotState as MoveitRobotState
+from moveit_msgs.srv import GetPositionFK, GetPositionIK
+
+from rclpy.node import Node
+
+from sensor_msgs.msg import JointState
+
 from typing import List
 
+
 class RobotState:
-    '''
-    Class that can compute forward or inverse kinematics of the robot in its current configuration or 
-    given the joint configuration/end-effector configuration respectively
-    '''
+    """Class that can compute forward or inverse kinematics of the robot."""
     def __init__(self, node: Node):
         self.node = node
         self.joint_state_sub = self.node.create_subscription(
@@ -19,20 +21,20 @@ class RobotState:
         self.joint_state = None
 
     def joint_state_callback(self, msg):
-        '''
-        Joint state publisher callback
-        
+        """
+        Joint state publisher callback.
+
         :param msg: Joint states of the robotic system
         :type msg: sensor_msgs.msg.JointState
-        '''
+        """
         self.joint_state = msg
 
     async def compute_IK(self,  
-                        goal_pose: PoseStamped, 
-                        joint_state: JointState = None, 
-                        group_name: str = 'fer_manipulator'):
-        '''
-        Computes inverse kinematics of the system given joint state or goal pose, or from current configuration
+                         goal_pose: PoseStamped, 
+                         joint_state: JointState = None, 
+                         group_name: str = 'fer_manipulator'):
+        """
+        Computes inverse kinematics of the system.
 
         :param goal_pose: goal position of end-effector
         :type goal_pose: geometry_msgs.msg.PoseStamped
@@ -42,7 +44,7 @@ class RobotState:
         :type group_name: string
         :returns: Inverse Kinematics solution
         :rtype: moveit_msgs/RobotState
-        '''
+        """
         if joint_state is None:
             joint_state = self.joint_state
         request = GetPositionIK.Request()
@@ -53,9 +55,9 @@ class RobotState:
         ik_solution = await self.ik_client.call_async(request)
         return ik_solution
 
-    async def compute_FK(self, link_names: List[str],joint_state: JointState = None):
-        '''
-        Computers forward kinematics of the system given joint states, or from current configuration
+    async def compute_FK(self, link_names: List[str], joint_state: JointState = None):
+        """
+        Computes forward kinematics of the system or from current configuration
          
         :param link_names: link  names to compute FK for
         :type link_names: list[string]
@@ -63,7 +65,7 @@ class RobotState:
         :type joint_state: sensor_msgs.msg.JointState
         :returns: Forward Kinematics solution
         :rtype: moveit_msgs/RobotState
-        '''
+        """
         if joint_state is None:
             joint_state = self.joint_state
         request = GetPositionFK.Request()
@@ -71,15 +73,15 @@ class RobotState:
         request.robot_state.joint_state = joint_state
         fk_solution = await self.fk_client.call_async(request)    
         return fk_solution
-        
+
     def get_robot_state(self) -> MoveitRobotState:
-        '''
-        Acquires current robot state
-        
+        """
+        Acquires current robot state.
+
         :returns: Robot state (moveit's msg definition)
         :rtype: moveit_msgs/RobotState
-        '''
+        """
         robot_state = MoveitRobotState()
         robot_state.joint_state = self.joint_state
-        return robot_state        
-        
+        return robot_state
+
--- object_mover/object_mover/pick_node.py
+++ object_mover/object_mover/pick_node.py
@@ -1,57 +1,55 @@
-'''
+"""
 The pick node is a node that accomplishes the pick and place task of an object by the robotic arm. It directly
 interacts with MotionPlanningInterface to access the functions of the objects of MotionPlanner, RobotState and 
 PlanningScene incorporated within the MotionPlanningInterface class. 
-'''
+"""
 
-import time
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Empty
 from object_mover.MotionPlanningInerface import MotionPlanningInterface
 from object_mover_interfaces.srv import PickPose
 from geometry_msgs.msg import Pose
-from enum import Enum, auto
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.action import ActionServer
 from moveit_msgs.action import MoveGroup
 import copy
 
+
 class State(Enum):
-    '''
-    Current state of the system (might not be needed)
-    '''
+    """Current state of the system (might not be needed)"""
     MOVING = auto()
     STATIONARY = auto()
 
 
 class PickNode(Node):
-    '''
-    Node that interacts with the moveit API to conduct pick and place task with robot arm
-    '''
+    """
+    Node that interacts with the moveit API to conduct pick and place task with robot arm.
+    """
     def __init__(self):
         super().__init__('pick_node')
         self._cbgroup = MutuallyExclusiveCallbackGroup()
-        self.serv = self.create_service(PickPose, 'pick', self.pick_callback, callback_group=MutuallyExclusiveCallbackGroup())
+        self.serv = self.create_service(PickPose,
+                                        'pick',
+                                        self.pick_callback,
+                                        callback_group=MutuallyExclusiveCallbackGroup())
         self._server = ActionServer(self,
                                     MoveGroup,
                                     '/viz/move_action',
                                     self.move_action_callback,
-                                    callback_group = self._cbgroup)
+                                    callback_group=self._cbgroup)
         self.state = State.STATIONARY
         self.mpi = MotionPlanningInterface(self)
 
-    # Maybe need to create custom service type for pick service as we want to be able to load multiple things to it
     async def pick_callback(self, request: PickPose, response):
-        '''
+        """
         Callback function when pick service is triggered
 
         :param request: service request object
         :type request: object_mover_interfaces.srv.PickPose
         :returns: response to service call
         :rtype: bool
-        '''
-        # For now I am assuming we are locating pose of object as the input to pick service 
+        """
+        # For now I am assuming we are locating pose of object as the input to pick service
         box_pose = Pose()
         box_pose.position.x = 0.59
         box_pose.position.y = 0.13
@@ -69,13 +67,13 @@ class PickNode(Node):
         pose1 = copy.deepcopy(object_pose)
 
         pose1.position.z = object_pose.position.z + 0.4
-        plan = await self.mpi.plan_path(goal_pose = pose1) 
+        plan=await self.mpi.plan_path(goal_pose=pose1)
         # await self.mpi.exec_path(path = plan)
 
         await self.mpi.motion_planner.toggle_gripper('open')
 
-        pose2 = object_pose
-        plan = await self.mpi.plan_path(goal_pose = pose2)
+        pose2=object_pose
+        plan=await self.mpi.plan_path(goal_pose=pose2)
         # await self.mpi.exec_path(path = plan)
 
         await self.mpi.motion_planner.toggle_gripper('close')
@@ -85,41 +83,30 @@ class PickNode(Node):
         await self.mpi.planning_scene.attach_object('box')
 
         # Lifts object slighty off table
-        pose3 = object_pose
-        pose3.position.z = object_pose.position.z + 0.3
-        plan = await self.mpi.plan_path(goal_pose = pose3)
-        # await self.mpi.exec_path(path = plan)
-        # Move arm to other side of obstacle
-        pose4 = object_pose
-        pose4.position.y = object_pose.position.y + 0.3
-        plan = await self.mpi.plan_path(goal_pose= pose4)
-        #await self.mpi.exec_path(path = plan)
+        pose3=object_pose
+        pose3.position.z=object_pose.position.z + 0.3
+        pose4=object_pose
+        pose4.position.y=object_pose.position.y + 0.3
+        #plan=await self.mpi.plan_path(goal_pose=pose4)
         await self.mpi.motion_planner.toggle_gripper('open')
         # Drop object
         return response
 
     async def move_action_callback(self, goal_handle):
-        self.get_logger().info("Move Action Called. Goal Is")
-        # self.get_logger().info(f"{goal_handle.request}") # Do more to format this output nicelynicely
-        self.get_logger().info(f"--- End ofrequest dump.----\n")
-        self.get_logger().info("Forwarding the action to the move_group action server")
-       
-        self.get_logger().info("Awaiting the result")
-        self.get_logger().info("Interception successful, returning the result")
+        self.get_logger().info('Move Action Called. Goal Is')
+        self.get_logger().info('--- End ofrequest dump.----\n')
+        self.get_logger().info('Forwarding the action')
+        self.get_logger().info('Awaiting the result')
+        self.get_logger().info('Interception successful, returning the result')
         return
 
 
 def pick_entry(args=None):
-    '''main function'''
     rclpy.init(args=args)
     node = PickNode()
     rclpy.spin(node)
-    rclpy.shutdown()  
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     pick_entry()
-
-
-
-
-
