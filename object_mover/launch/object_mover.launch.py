cf4ad923fa2276d48f6c45e33c429f4e7243b64e
haodong <haodong0518@gmail.com>
2024-11-21 17:26:32 -0600

    fix RoboState, Launch, MPI

--- object_mover/launch/object_mover.launch.py
+++ object_mover/launch/object_mover.launch.py
@@ -1,47 +1,55 @@
-from moveit_configs_utils import MoveItConfigsBuilder
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
+from launch.substitutions import EqualsSubstitution, LaunchConfiguration, \
+    PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution, EqualsSubstitution, \
-                                    LaunchConfiguration
+
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder('fer', package_name='franka_fer_moveit_config').to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        'fer',
+        package_name='franka_fer_moveit_config'
+    ).to_moveit_configs()
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'debug',
             default_value='false',
-            description='Boolean flag to indicuate production mode: true or false.'
+            description='Boolean flag to indicate production mode: true or false.'
         ),
-        Node(package='rviz2',
-            condition=IfCondition(EqualsSubstitution(LaunchConfiguration('debug'), 'true')),
+        Node(
+            package='rviz2',
+            condition=IfCondition(
+                EqualsSubstitution(LaunchConfiguration('debug'), 'true')
+            ),
             executable='rviz2',
             output='log',
-            arguments=['-d',
-                       PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),
-                       'config',
-                       'moveit.rviz'])],
-            parameters = [ moveit_config.planning_pipelines, moveit_config.robot_description_kinematics],
-            # Remapping actions not implemented, must remap individual topics/services
-            # https://github.com/ros2/ros2/issues/1312 Accessed on 10/30/2024
-            remappings = [('/move_action/_action/feedback',
-                           '/viz/move_action/_action/feedback'),
-                           ('/move_action/_action/status',
-                            '/viz/move_action/_action/status'),
-                           ('/move_action/_action/cancel_goal',
-                            '/viz/move_action/_action/cancel_goal'),
-                           ('/move_action/_action/get_result',
-                            '/viz/move_action/_action/get_result'),
-                           ('/move_action/_action/send_goal',
-                            '/viz/move_action/_action/send_goal'
-                            ),
-                           ]
-            ),
-        Node(package='object_mover',
-             executable='pick_node',
-             )
-        ])
+            arguments=[
+                '-d',
+                PathJoinSubstitution([
+                    FindPackageShare('franka_fer_moveit_config'),
+                    'config',
+                    'moveit.rviz'
+                ])
+            ],
+            parameters=[
+                moveit_config.planning_pipelines,
+                moveit_config.robot_description_kinematics
+            ],
+            remappings=[
+                ('/move_action/_action/feedback', '/viz/move_action/_action/feedback'),
+                ('/move_action/_action/status', '/viz/move_action/_action/status'),
+                ('/move_action/_action/cancel_goal', '/viz/move_action/_action/cancel_goal'),
+                ('/move_action/_action/get_result', '/viz/move_action/_action/get_result'),
+                ('/move_action/_action/send_goal', '/viz/move_action/_action/send_goal')
+            ]
+        ),
+        Node(
+            package='object_mover',
+            executable='pick_node'
+        )
+    ])
